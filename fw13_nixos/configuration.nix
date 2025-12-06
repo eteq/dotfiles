ec4b85cfbedc9da792bf720625a024d1e3b0c0ae
Erik Tollerud <erik.tollerud@gmail.com>
2025-12-05 20:16:42 -0500

    update for 25.11

--- fw13_nixos/configuration.nix
+++ fw13_nixos/configuration.nix
@@ -109,10 +109,11 @@ in
       enable32Bit = lib.mkDefault true;
     };
 
-    amdgpu.amdvlk = {
-      enable = lib.mkDefault false;
-      support32Bit.enable = lib.mkDefault false;
-    };
+    # disabled in 25.11
+    #amdgpu.amdvlk = {
+    #  enable = lib.mkDefault false;
+    #  support32Bit.enable = lib.mkDefault false;
+    #};
     i2c.enable = true;
   };
  
@@ -158,15 +159,15 @@ in
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    #package = config.boot.kernelPackages.nvidiaPackages.stable;
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "580.95.05";
-      sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
-      sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
-      openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
-      settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
-      persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
-    };
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    #  version = "580.95.05";
+    #  sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+    #  sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
+    #  openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
+    #  settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
+    #  persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
+    #};
     
     prime = {
       sync.enable = lib.mkDefault false;
@@ -330,7 +331,8 @@ in
     gamemode
 
     libreoffice-fresh
-    onlyoffice-bin
+    #onlyoffice-bin
+    onlyoffice-desktopeditors
 
     # qt/KDE applications
     kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
